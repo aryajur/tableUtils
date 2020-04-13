3232ca7f2111b926c9118d7addb86b627ba4f0b6
Milind Gupta <milind.gupta@gmail.com>
2020-04-13 04:01:36 -0700

    Improved compareTables function to search for table keys that match

--- src/tableUtils.lua
+++ src/tableUtils.lua
@@ -208,7 +208,7 @@ end
 -- 8. table
 -- The table to string and string to table conversion will maintain the following types:
 -- nil, boolean, number, string, table
--- The other types get their tostring values stored and end up as a string ID.
+-- The other three types (function, userdata and thread) get their tostring values stored and end up as a string ID.
 function t2sr(t)
 	if type(t) ~= 'table' then return nil, 'Expected table parameter' end 
 	local rL = {cL = 1}	-- Table to track recursion into nested tables (cL = current recursion level)
@@ -352,34 +352,93 @@ function s2tr(str)
 	return safeenv.t0
 end
 
-function compareTables(t1,t2)
+
+-- Function to compare 2 tables. Returns nil if they are not equal in value
+-- Recursive tables are allowed
+function compareTables(t1,t2,traversed)
+	traversed = traversed or {}
+	traversed[t1] = true
+	local donet2 = {}	-- To mark wchich keys are taken	
 	for k,v in pairs(t1) do
 		--print(k,v)
 		if type(v) == "number" or type(v) == "string" or type(v) == "boolean" or type(v) == "function" or type(v) == "thread" or type(v) == "userdata" then
-			if v ~= t2[k] then
-				--print("Value "..tostring(v).." does not match")
-				return nil
+			if type(k) == "table" then
+				-- Find a matching key
+				local found
+				for k2,v2 in pairs(t2) do
+					if not donet2[k2] and type(k2) == "table" then
+						if (traversed[k2] or compareTables(k,k2,traversed)) and v2 == v then
+							found = k2
+							break
+						end
+					end
+				end
+				if not found then
+					return false
+				end
+				donet2[found] = true
+			else
+				if v ~= t2[k] then
+					return false
+				end
+				donet2[k] = true
 			end
 		else
 			-- type(v) = ="table"
 			--print("-------->Going In "..tostring(v))
-			if not compareTables(v,t2[k]) then
-				return nil
+			if type(k) == "table" then
+				-- Find a matching key
+				local found
+				for k2,v2 in pairs(t2) do
+					if not donet2[k2] and type(k2) == "table" then
+						if (traversed[k2] or compareTables(k,k2,traversed)) and (traversed[v2] or compareTables(v,v2,traversed)) then
+							found = k2
+							break
+						end
+					end
+				end
+				if not found then
+					return false
+				end
+				donet2[found] = true
+			else
+				if not (traversed[t2[k]] or compareTables(v,t2[k],traversed)) then
+					return false
+				end
+				donet2[k] = true
 			end
+		end
+	end
+	-- Check if any keys left in t2
+	for k,v in pairs(t2) do
+		if not donet2[k] then
+			return false	-- extra stuff in t2
 		end
 	end
 	return true
 end
 
+
 -- Copy table t1 to t2 overwriting any common keys
 -- If full is true then copy is recursively going down into nested tables
 -- returns t2 
-function copyTable(t1,t2,full)
+function copyTable(t1,t2,full,map)
+	map = map or {
+			s2d={
+				[t1]=t2
+			},
+			d2s={
+				[t2]=t1
+			}
+		}	-- s2d contains mapping of source table tables to destination tables
+			-- d2s contains mapping of destination table tables to source tables
 	for k,v in pairs(t1) do
 		if type(v) == "number" or type(v) == "string" or type(v) == "boolean" or type(v) == "function" or type(v) == "thread" or type(v) == "userdata" then
 			if type(k) == "table" and full then
 				local kp = {}
-				copyTable(k,kp,true)
+				copyTable(k,kp,true,map)
+				map.d2s[kp] = k
+				map.s2d[k] = kp
 				t2[kp] = v
 			else
 				t2[k] = v
@@ -389,19 +448,141 @@ function copyTable(t1,t2,full)
 			if full then 
 				if type(k) == "table" then
 					local kp = {}
-					copyTable(k,kp,true)
+					copyTable(k,kp,true,map)
+					map.d2s[kp] = k
+					map.s2d[k] = kp
 					t2[kp] = {}
-					copyTable(v,t2[kp],true)
+					copyTable(v,t2[kp],true,map)
+					map.d2s[t2[kp]] = v
+					map.s2d[v] = t2[kp]
 				else
 					t2[k] = {}
-					copyTable(v,t2[k],true)
+					copyTable(v,t2[k],true,map)
+					map.d2s[t2[k]] = v
+					map.s2d[v] = t2[k]
 				end
 			else
 				t2[k] = v
 			end
 		end
 	end
-	return t2
+	return t2,map
+end
+
+local setnil = {}	-- Marker table for diff to set nil
+
+-- Function to patch table t with the diff provided to convert it to the next table
+-- diff is a structure as returned by the diffTable function
+function patch(t,diff,tabDone)
+	local tabDone = tabDone or {[t]=true}
+	for k,v in pairs(diff[t]) do
+		t[k] = v
+	end
+	for k,v in pairs(diff[t]) do
+		if type(k) == "table" and not tabDone[k] then
+			patch(k,diff,tabDone)
+		end
+		if type(v) == "table" and not tabDone[k] then
+			patch(v,diff,tabDone)
+		end
+	end
+	return true
+end
+
+-- Function to return the diff patch of t2-t1. The patch when applied to t1 will make it equal in value to t2 such that compareTables will return true
+-- Use the patch function the apply the patch
+-- map is the table that can provide mapping of any table in t2 to a table in t1 i.e. they can be considered the referring to the same table i.e. that table in t2 after the patch operation would be the same in value as the table in t1 that the map defines but its address will still be the address it was in t2. If there is no mapping for the table found then the same table is looked up at that level to match. But if there is a same table then the diff for that table is obviously 0
+
+-- NOTE: a diff object is temporary and cannot be saved for a later session. To save it is better to serialize and save t1 and t2 using t2s functions
+function diffTable(t1,t2,map,tabDone)
+	map = map or {
+			[t2]=t1
+		}
+	tabDone = tabDone or {[t2]=true}	-- To keep track of recursive tables
+	local diff = {}
+	local keyTabs = {}
+	-- To convert t1 to t2 let us iterate over all elements of t2 first
+	for k,v in pairs(t2) do
+		-- There are 8 types in Lua (except nil and table we check everything here
+		if type(v) ~= "table" then			--
+			if type(k) == "table" then
+				-- Check if there is a mapping else the mapping in t1 is k
+				local kt1 = k
+				if map[k] then
+					kt1 = map[k]
+					-- Get diff of kt1 and k
+					if not tabDone[k] then
+						diff[kt1] = diffTable(kt1,k,map,tabDone)
+					end
+				end
+				keyTabs[kt1] = k
+				if t1[kt1] == nil or t1[kt1] ~= v then
+					diff[t1] = diff[t1] or {}
+					diff[t1][kt1] = v 
+				end
+			else	-- if type(k) == "table" then else
+				-- Neither v is a table not k is a table
+				if t1[k] ~= v then
+					diff[t1] = diff[t1] or {}
+					diff[t1][k] = v
+				end				
+			end		-- if type(k) == "table" then ends
+		else	--if type(v) ~= "table" then	
+			-- v == "table"
+			if type(k) == "table" then
+				-- Both v and k are tables
+				local kt1 = k
+				if map[k] then
+					kt1 = map[k]
+					if not tabDone[k] then
+						diff[kt1] = diffTable(kt1,k,map,tabDone)
+					end
+				end
+				keyTabs[kt1] = k
+				local vt1 = v
+				if map[v] then
+					vt1 = map[v]
+					if not tabDone[v] then
+						diff[vt1] = diffTable(vt1,v,map,tabDone)
+					end
+				end
+				if t1[kt1] == nil or t1[kt1] ~= vt1 then
+					diff[t1] = diff[t1] or {}
+					diff[t1][kt1] = vt1
+				end
+			else
+				local vt1 = v
+				if map[v] then
+					vt1 = map[v]
+					-- Get the diff of vt1 and v
+					if not tabDone[v] then
+						diff[vt1] = diffTable(vt1,v,map,tabDone)
+					end
+				end
+				if t1[k] == nil or t1[k] ~= vt1 then
+					diff[t1] = diff[t1] or {}
+					diff[t1][k] = vt1
+				end
+			end
+		end	--if type(v) ~= "table" then ends
+	end	-- for k,v in pairs(t2) do ends
+	-- Now to find extra stuff in t1 which should be removed
+	for k,v in pairs(t1) do
+		if type(k) ~= "table" then
+			if t2[k] == nil then
+				diff[t1] = diff[t1] or {}
+				diff[t1][k] = setnil
+			end
+		else
+			-- k is a table 
+			-- get the t2 counterpart if it was found
+			if not keyTabs[k] then
+				diff[t1] = diff[t1] or {}
+				diff[t1][k] = setnil
+			end
+		end
+	end
+	return diff
 end
 
 -- Merge arrays t1 to t2
--- test/test1.lua
+++ test/test1.lua
@@ -17,6 +17,23 @@ print(tu.t2s({
 			[{me=true}]="test"
 		}))
 		
+print("Result of compareTables:")
+print(tu.compareTables(tu.s2t(tu.t2s({
+			[1]="Hello",
+			[2]="World",
+			Hello="World",
+			x = {},
+			[{me=true}]="test",
+			[{me=false}]="test"
+		})),{
+			[1]="Hello",
+			[2]="World",
+			Hello="World",
+			x = {},
+			[{me=false}]="test",
+			[{me=true}]="test",
+		}))
+		
 print("Now table to string with pretty print")
 print(tu.t2spp({
 			[1]="Hello",
